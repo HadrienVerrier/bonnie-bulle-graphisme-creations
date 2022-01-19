15dd8848e2cad45f820c6567c889a29d08abe548
Hadrien Verrier <hadverrier@gmail.com>
2022-01-19 14:39:34 +0100

    update website on new WSL

--- client/functions/helpers/airtable.js
+++ client/functions/helpers/airtable.js
@@ -1,8 +1,6 @@
-require("dotenv").config();
-
 var Airtable = require("airtable");
 var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-	process.env.AIRTABLE_BASE_ID
+  process.env.AIRTABLE_BASE_ID
 );
 
 const imagesTable = base("images");
--- client/functions/server.js
+++ client/functions/server.js
@@ -1,14 +1,15 @@
+require("dotenv").config();
 const formattedReturn = require("./helpers/formattedReturn");
 const getDatabase = require("./helpers/getDatabase");
 const postDatabase = require("./helpers/postDatabase");
 
 exports.handler = async (event) => {
-	switch (event.httpMethod) {
-		case "GET":
-			return await getDatabase(event);
-		case "POST":
-			return await postDatabase(event);
-		default:
-			return formattedReturn(405, { message: "No event provided" });
-	}
+  switch (event.httpMethod) {
+    case "GET":
+      return await getDatabase(event);
+    case "POST":
+      return await postDatabase(event);
+    default:
+      return formattedReturn(405, { message: "No event provided" });
+  }
 };
